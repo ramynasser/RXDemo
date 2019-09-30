502285c1fe1404d80bb89fad4c07edb3d901d3ac
Ramy Nasser <ramy.assr@vodafone.com>
2019-09-30 12:07:42 +0200

    Initial Commit

---
+++ universityFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  universityFinder
+//
+//  Created by Ramy Nasser on 9/30/19.
+//  Copyright © 2019 Ramy Nasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
